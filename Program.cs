fda73d512e8ebd28e58a026b29824634a4a65a19
Yousef Shahadeh <yousef.shahadeh@icloud.com>
2022-10-11 22:30:12 +0200

    LINQ added

--- Program.cs
+++ Program.cs
@@ -13,8 +13,15 @@ namespace SuperDevOps
             DelegatenKlasse.WeiterleitenAnProgramklasse();
             
             LambdaAusdrücken.Begrüßung('A');
-            
-            
+            string[] words = { "apple", "strawberry", "grape", "peach", "banana" };
+            var wordQuery = from word in words
+                            where word[0] == 's'
+                            select word;
+            foreach (var item in wordQuery)
+            {
+                Console.WriteLine(item);
+            }
+
 
         }
     }
